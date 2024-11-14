b23813f186e98e57056c3394546b1536dc5c56e5
Yihan Xie <yxie389@uwo.ca>
2024-11-14 15:28:10 -0500

    adding Product cards

--- client/src/components/home/ItemList.jsx
+++ client/src/components/home/ItemList.jsx
@@ -1,5 +1,9 @@
 
 import iMacImage from "../../static/images/iMac.png";
+import MacProImage from "../../static/images/MacPro.png";
+import AirPodsMaxImage from "../../static/images/AirPodsMax.png";
+import HomePodMiniImage from "../../static/images/HomePodMini.png";
+
 
 const ITEM_LIST = [
   {
@@ -13,21 +17,30 @@ const ITEM_LIST = [
   },
   {
     id: 2,
-    name: "iMac",
+    name: "Macbook Pro",
     manufacturer: "Apple",
-    title: "iMac - $1,699.00",
+    title: "Macbook Pro - $2,699.00",
     description:
-      "Image Playground. Create fun, original images based on a description, a concept or even a person from your Photos library.",
-    image: iMacImage,
+      "MacBook Pro features the most advanced lineup of chips ever built for a pro laptop.",
+    image: MacProImage,
   },
   {
     id: 3,
-    name: "iMac",
+    name: "AirPods Max",
     manufacturer: "Apple",
-    title: "iMac - $1,699.00",
+    title: "AirPods Max - $779.00",
     description:
-      "Image Playground. Create fun, original images based on a description, a concept or even a person from your Photos library.",
-    image: iMacImage,
+      "AirPods Max deliver stunningly detailed, high‑fidelity audio. Personalised Spatial Audio with dynamic head tracking for sound that surrounds you. ",
+    image: AirPodsMaxImage,
+  },
+  {
+    id: 4,
+    name: "HomePod Mini",
+    manufacturer: "Apple",
+    title: "HomePod Mini - $129.00",
+    description:
+      "Jam-packed with innovation, HomePod mini delivers unexpectedly big sound for a speaker of its size. ",
+    image: HomePodMiniImage,
   },
 ];
 
